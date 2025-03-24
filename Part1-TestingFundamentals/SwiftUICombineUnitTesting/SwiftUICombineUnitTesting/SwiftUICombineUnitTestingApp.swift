bcfff97e85342895cf08539aed3c33a6bd1878f9
eastsunshinee <eastsunshinee@gmail.com>
2025-03-24 17:40:13 +0900

    Init commit - 프로젝트 기본 구조 및 .gitignore 추가

---
+++ Part1-TestingFundamentals/SwiftUICombineUnitTesting/SwiftUICombineUnitTesting/SwiftUICombineUnitTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICombineUnitTestingApp.swift
+//  SwiftUICombineUnitTesting
+//
+//  Created by 김동현 on 3/24/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICombineUnitTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Part2-UIAndIntegrationTesting/SwiftUIXCUITest/SwiftUIXCUITest/SwiftUIXCUITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIXCUITestApp.swift
+//  SwiftUIXCUITest
+//
+//  Created by 김동현 on 3/24/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIXCUITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
