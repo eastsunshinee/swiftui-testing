f6077173a3295ab4e7323b06a66c0f9f17918abe
eastsunshinee <eastsunshinee@gmail.com>
2025-03-24 17:36:15 +0900

    Init commit - 프로젝트 기본 구조 및 .gitignore 추가

---
+++ Part1-TestingFundamentals/SwiftUITestingEssentials/SwiftUITestingEssentials/Models/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  SwiftUITestingEssentials
+//
+//  Created by 김동현 on 3/24/25.
+//
+
+import Foundation
+
+struct User: Codable {
+    let id: String
+    let name: String
+}
---
+++ Part1-TestingFundamentals/SwiftUITestingEssentials/SwiftUITestingEssentials/SwiftUITestingEssentialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITestingEssentialsApp.swift
+//  SwiftUITestingEssentials
+//
+//  Created by 김동현 on 3/24/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITestingEssentialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
